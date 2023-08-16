d3234ee8ef02cfbd0ec47f307dd9985ca65e9218
DANK??? <96366744+DANK1175@users.noreply.github.com>
2023-08-16 21:48:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="pros.php" method="post">
+        <label for="nombre">nombre</label>
+        <input type="text" name="Ncompleto" id="nombre">
+        <label for="fecha"></label>
+        <input type="date" name="datefecha" id="fecha">
+        <input type="submit">
+    </form>
+</body>
+</html>
